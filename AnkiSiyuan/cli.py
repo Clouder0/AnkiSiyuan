80e300cfc752a1352f541b575eb06a3ca914cf4e
clouder <clouder0@outlook.com>
2021-08-23 12:50:37 +0800

    async

--- AnkiSiyuan/cli.py
+++ AnkiSiyuan/cli.py
@@ -4,40 +4,45 @@ from AnkiIn.helper import ankiConnectHelper as ankihelper
 import config
 import config_parser
 import datetime
+import aiohttp
+import asyncio
 
 
-def execute_from_commandline():
+async def execute_from_commandline():
+    session = aiohttp.ClientSession()
+    siyuanhelper.set_session(session)
     config_parser.parse()
     print(config.auth_code)
     print(config.last_sync_time)
     this_sync_time = datetime.datetime.now().strftime(r"%Y%m%d%H%M%S")
     print(this_sync_time)
-    noteList = parser.sync(config.last_sync_time)
+    noteList = await parser.sync(config.last_sync_time)
     updated_notes = 0
     added_notes = 0
     for x in noteList:
         if x is None:
             continue
         # print(x.fields)
-        old = ankihelper.find_notes("SiyuanID:\"{}\"".format(x.outputfields["SiyuanID"]))
+        old = await ankihelper.find_notes("SiyuanID:\"{}\"".format(x.outputfields["SiyuanID"]))
         # print(x.outputfields["SiyuanID"])
         # print(x.SiyuanID)
         if len(old) >= 2:
             print("Found SiyuanID[{}] with duplicates in Anki!\n{}".format(
                 x["SiyuanID"], old))
         elif len(old) == 1:
-            ankihelper.update_note_fields(old[0], x)
+            await ankihelper.update_note_fields(old[0], x)
             print("Updated: SiyuanID[{}] AnkiID[{}]".format(
                 x.fields["SiyuanID"], old[0]))
             updated_notes = updated_notes + 1
         else:
-            ankihelper.add_note(x)
+            await ankihelper.add_note(x)
             added_notes = added_notes + 1
     print("added {} notes, updated {} notes.".format(added_notes, updated_notes))
     f = open("last_sync_time", "w", encoding="UTF-8")
     f.write(this_sync_time)
     f.close()
+    await session.close()
 
 
 if __name__ == "__main__":
-    execute_from_commandline()
+    asyncio.run(execute_from_commandline())
