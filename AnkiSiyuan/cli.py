ff7cdd6466ae84cf278b60a3cddf5328f5623a37
clouder <clouder0@outlook.com>
2021-08-29 12:31:39 +0800

    update

--- AnkiSiyuan/cli.py
+++ AnkiSiyuan/cli.py
@@ -6,9 +6,15 @@ import config_parser
 import datetime
 import aiohttp
 import asyncio
+import time
+
+
+updated_notes = 0
+added_notes = 0
 
 
 async def execute_from_commandline():
+    start_time = time.perf_counter()
     session = aiohttp.ClientSession()
     siyuanhelper.set_session(session)
     config_parser.parse()
@@ -17,31 +23,35 @@ async def execute_from_commandline():
     this_sync_time = datetime.datetime.now().strftime(r"%Y%m%d%H%M%S")
     print(this_sync_time)
     noteList = await parser.sync(config.last_sync_time)
-    updated_notes = 0
-    added_notes = 0
     for x in noteList:
         if x is None:
             continue
-        # print(x.fields)
-        old = await ankihelper.find_notes("SiyuanID:\"{}\"".format(x.outputfields["SiyuanID"]))
-        # print(x.outputfields["SiyuanID"])
-        # print(x.SiyuanID)
-        if len(old) >= 2:
-            print("Found SiyuanID[{}] with duplicates in Anki!\n{}".format(
-                x["SiyuanID"], old))
-        elif len(old) == 1:
-            await ankihelper.update_note_fields(old[0], x)
-            print("Updated: SiyuanID[{}] AnkiID[{}]".format(
-                x.fields["SiyuanID"], old[0]))
-            updated_notes = updated_notes + 1
-        else:
-            await ankihelper.add_note(x)
-            added_notes = added_notes + 1
+        handle(x)
     print("added {} notes, updated {} notes.".format(added_notes, updated_notes))
+    end_time = time.perf_counter()
+    print("Time consumed: {}s".format(end_time - start_time))
     f = open("last_sync_time", "w", encoding="UTF-8")
     f.write(this_sync_time)
     f.close()
     await session.close()
+
+
+def handle(x):
+    global updated_notes, added_notes
+    old = ankihelper.find_notes("SiyuanID:\"{}\"".format(x.outputfields["SiyuanID"]))
+    # print(x.outputfields["SiyuanID"])
+    # print(x.SiyuanID)
+    if len(old) >= 2:
+        print("Found SiyuanID[{}] with duplicates in Anki!\n{}".format(
+            x["SiyuanID"], old))
+    elif len(old) == 1:
+        ankihelper.update_note_fields(old[0], x)
+        print("Updated: SiyuanID[{}] AnkiID[{}]".format(
+            x.fields["SiyuanID"], old[0]))
+        updated_notes = updated_notes + 1
+    else:
+        ankihelper.add_note(x)
+        added_notes = added_notes + 1
 
 
 if __name__ == "__main__":
